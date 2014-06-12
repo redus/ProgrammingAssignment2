38ff49f9e3aa01bcebbc11d7670b3b3176230a34
Jin Yang <redus891@gmail.com>
2014-06-12 21:57:41 +0900

    add sample.R, update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author: redus
+## Desc: Compute and cache the inverse of an invertible square matrix.
 
-## Write a short comment describing this function
-
+## REQ: x is an invertible square matrix
+## EFF: set list of fns to inverse and cache in parent env.
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	set_inverse <- function(inverse) inv <<- inverse
+	get_inverse <- function() inv
+	list(set = set, get = get, 
+		set_inverse = set_inverse, 
+		get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
-
+## REQ: x is output of makeCacheMatrix() 
+## EFF: return the inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$get_inverse()
+	if (!is.null(inv)){
+		# message("cached")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$set_inverse(inv)
+	inv
 }
